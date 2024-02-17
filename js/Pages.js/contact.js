9661e78c5d1c3561c896f4ee73c5d81648e2e053
Berrinj <69366646+Berrinj@users.noreply.github.com>
2024-02-17 12:58:20 +0100

    switch statements in index.js
    Trying to route my locations to the correct .js file from index.js. working on local live, let's see for the deployed site now..
    Also removed <script ++index.js from the static pages unitil the day I'll fetch them from the api as well.

--- js/Pages.js/contact.js
+++ js/Pages.js/contact.js
@@ -101,94 +101,14 @@ console.log("Form Data:", body)
 
 form.addEventListener("submit", formSubmissionHandler);
 
+/*      https://css-tricks.com/headless-form-submission-with-the-wordpress-rest-api/        */
 
-/*          KODEN TIL FORRIGE KONATKT SKJEMA UTEN API       */
 
-// const form = document.getElementById("form");
-// const fieldset = document.querySelector("fieldset");
-// const name = document.getElementById("name");
-// const email = document.getElementById("email");
-// const subject = document.getElementById("subject");
-// const message = document.getElementById("message");
+// const nav = document.querySelector(".main-menu");
+// const hamburgerMenu = document.querySelector(".hamburger-menu");
 
-// form.addEventListener("submit", e => {
-//     e.preventDefault();
+// hamburgerMenu.addEventListener("click", hamburgerMenuClick);
 
-//     if(validateInputs()) {
-//         form.reset();
-//         fieldset.style.background = "#a1ca77";
-//         fieldset.innerHTML = `<p class="success-form-message">Meldingen din er mottatt, jeg prøver å svare innen 24timer :) </p><p><i>Kontaktskjema er ikke i bruk enda</i>.</p>`;
-//     };
-// });
-
-// const setError = (element, message) => {
-//     const inputControl = element.parentElement;
-//     const errorMessage = inputControl.querySelector(".form-error");
-//     // inputControl.classList.add("form-error");
-//     // inputControl.classList.remove("success");
-//     errorMessage.innerText = message;
+// export function hamburgerMenuClick() {
+//     nav.classList.toggle("active")
 // }
-
-// const setSuccess = element => {
-//     const inputControl = element.parentElement;
-//     const errorMessage = inputControl.querySelector(".form-error");
-
-//     errorMessage.innerText = "";
-//     inputControl.classList.add("success")
-//     inputControl.classList.remove("form-error")
-// }
-
-// const isEmailValid = email => {
-//     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//     return re.test(String(email).toLowerCase());
-// };
-
-// const validateInputs = () => {
-//     let isValid = true;
-//     const nameValue = name.value.trim();
-//     const emailValue = email.value.trim();
-//     const subjectValue = subject.value.trim();
-//     const messageValue = message.value.trim();
-
-//    if (nameValue.length < 6) {
-//         setError(name, "Navnet må være på over 5 karakterer");
-//         isValid = false;
-//     } else {
-//         setSuccess(name);
-//     }
-
-//     if (emailValue === "") {
-//         setError(email, "Email er påkrevd");
-//         isValid = false;
-//     } else if (!isEmailValid(emailValue)) {
-//         setError(email, "Skriv inn en ekte email adresse");
-//         isValid = false;
-//     }else {
-//         setSuccess(email);
-//     }
-
-//     if (subjectValue.length < 16) {
-//         setError(subject, "Emnet må være på over 15 karakterer");
-//         isValid = false;
-//     } else {
-//         setSuccess(subject);
-//     }
-    
-//     if (messageValue.length < 26) {
-//         setError(message, "Meldingen må være på over 25 karakterer");
-//         isValid = false;
-//     } else {
-//         setSuccess(message);
-//     }
-
-//     return isValid;
-// };
-
-const nav = document.querySelector(".main-menu");
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-
-hamburgerMenu.addEventListener("click", hamburgerMenuClick);
-
-export function hamburgerMenuClick() {
-    nav.classList.toggle("active")
-}
--- js/Pages.js/index.js
+++ js/Pages.js/index.js
@@ -2,6 +2,7 @@
 // import { hamburgerMenuClick } from "../components/hamburgerMenu.js";
 // hamburgerMenuClick();
 
+ /* Hamburger Menu  */
 const nav = document.querySelector(".main-menu");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 
@@ -10,3 +11,32 @@ hamburgerMenu.addEventListener("click", hamburgerMenuClick);
 export function hamburgerMenuClick() {
     nav.classList.toggle("active")
 }
+
+switch (window.location.pathname) {
+    case "/index.html":
+        import("../fetchAPI/getPost.js").then((module) => {
+           console.log('Loaded getPost.js'); 
+        }) 
+        break;
+    case "/contact.html":
+        case "/contact":
+      import("./contact.js").then((module) => {
+        // Do something with the imported module
+        console.log('Loaded contact.js');
+      });
+      break;
+    case "/category.html":
+      import("./category.js").then((module) => {
+        // Do something with the imported module
+        console.log('Loaded category.js');
+      });
+      break;
+    case "/single-post.html":
+      import("./singlePost.js").then((module) => {
+        // Do something with the imported module
+        console.log('Loaded singlePost.js');
+      });
+      break;
+    default:
+      console.warn(`No script defined for the path: ${window.location.pathname}`);
+  }
