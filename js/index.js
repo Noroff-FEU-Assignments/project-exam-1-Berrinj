373f3c1576302501186fd9d3a2c9a49a1225658a
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-03-09 18:13:28 +0100

    Now I dared changing the name of that misspelled folder, that was not an option during the exam in case of full breakdown

--- js/index.js
+++ js/index.js
@@ -4,27 +4,23 @@ const hamburgerMenu = document.querySelector(".hamburger-menu");
 hamburgerMenu.addEventListener("click", hamburgerMenuClick);
 
 export function hamburgerMenuClick() {
-    nav.classList.toggle("active")
+  nav.classList.toggle("active");
 }
 
 switch (window.location.pathname) {
-    case "/index.html":
-        case "/":
-        import("../js/fetchAPI/getPost.js").then((module) => {
-        }) 
-        break;
-    case "/contact.html":
-        case "/contact":
-      import("../js/Pages.js/contact.js").then((module) => {
-      });
-      break;
-    case "/category.html":
-      import("../js/Pages.js/category.js").then((module) => {
-      });
-      break;
-    case "/single-post.html":
-      import("../js/Pages.js/singlePost.js").then((module) => {
-      });
-      break;
-    default:
-  }
+  case "/index.html":
+  case "/":
+    import("../js/fetchAPI/getPost.js").then((module) => {});
+    break;
+  case "/contact.html":
+  case "/contact":
+    import("../js/pages/contact.js").then((module) => {});
+    break;
+  case "/category.html":
+    import("../js/pages/category.js").then((module) => {});
+    break;
+  case "/single-post.html":
+    import("../js/pages/singlePost.js").then((module) => {});
+    break;
+  default:
+}
